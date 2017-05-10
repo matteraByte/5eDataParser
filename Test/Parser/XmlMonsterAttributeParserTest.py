dde18d5520f20a03c1dfa35996f5a5790043a49e
Chris Mattera <chris.mattera@comcast.com>
2017-05-10 11:40:29 -0700

    Parsing Monster actions. Added associated tests

--- Test/Parser/XmlMonsterAttributeParserTest.py
+++ Test/Parser/XmlMonsterAttributeParserTest.py
@@ -49,15 +49,70 @@ class XmlMonsterAttributeParserTest(unittest.TestCase):
         missing_special_abilities = self.parser_missing_fields.get_special_abilities()
         self.assertEquals(len(missing_special_abilities), 0)
 
+    def test_get_attack_bonus_from_attack_string(self):
+        attack_string_normal = "Tail|9|3d6+5"
+        expected_normal = 9
+        attack_string_weird = "Tail||3d6"
+        expected_weird = 0
+        attack_string_empty = ""
+        expected_empty = 0
+        self.assertEquals(self.parser.get_attack_bonus_from_attack_string(attack_string_empty), expected_empty)
+        self.assertEquals(self.parser.get_attack_bonus_from_attack_string(attack_string_weird), expected_weird)
+        self.assertEquals(self.parser.get_attack_bonus_from_attack_string(attack_string_normal), expected_normal)
+
+    def test_get_damage_bonus_from_attack_string(self):
+        attack_string_normal = "Tail|9|3d6+5"
+        expected_normal = 5
+        attack_string_no_bonus = "Tail|9|3d6"
+        expected_no_bonus = 0
+        attack_string_weird = "Tail||"
+        expected_weird = 0
+        attack_string_empty = ""
+        expected_empty = 0
+        self.assertEquals(self.parser.get_damage_bonus_from_attack_string(attack_string_empty), expected_empty)
+        self.assertEquals(self.parser.get_damage_bonus_from_attack_string(attack_string_weird), expected_weird)
+        self.assertEquals(self.parser.get_damage_bonus_from_attack_string(attack_string_normal), expected_normal)
+        self.assertEquals(self.parser.get_damage_bonus_from_attack_string(attack_string_no_bonus), expected_no_bonus)
+
+    def test_get_damage_dice_from_attack_string(self):
+        attack_string_normal = "Tail|9|3d6+5"
+        expected_normal = "3d6"
+        attack_string_no_bonus = "Tail|9|3d6"
+        expected_no_bonus = "3d6"
+        attack_string_weird = "Tail||"
+        expected_weird = ""
+        attack_string_empty = ""
+        expected_empty = ""
+        self.assertEquals(self.parser.get_damage_dice_from_attack_string(attack_string_empty), expected_empty)
+        self.assertEquals(self.parser.get_damage_dice_from_attack_string(attack_string_weird), expected_weird)
+        self.assertEquals(self.parser.get_damage_dice_from_attack_string(attack_string_normal), expected_normal)
+        self.assertEquals(self.parser.get_damage_dice_from_attack_string(attack_string_no_bonus), expected_no_bonus)
+
     def test_get_actions(self):
         actions = self.parser.get_actions()
         self.assertNotEquals(len(actions), 0)
         action1 = actions[0]
         action2 = actions[1]
+        # action3 = actions[2]
+        action4 = actions[3]
         self.assertEquals(action1.name, "Multiattack")
-        self.assertEquals(action1.description, "Melee Weapon Attack: +9 to hit, reach 10 ft., one target. Hit: 12 (2d6 + 5) bludgeoning damage. If the target is a creature, it must succeed on a DC 14 Constitution saving throw or become diseased. The disease has no effect for 1 minute and can be removed by any magic that cures disease. After 1 minute, the diseased creature's skin becomes translucent and slimy, the creature can't regain hit points unless it is underwater, and the disease can be removed only by heal or another disease-curing spell of 6th level or higher. When the creature is outside a body of water, it takes 6 (1d12) acid damage every 10 minutes unless moisture is applied to the skin before 10 minutes have passed.")
-        self.assertEquals(action1.damage_dice, "2d6")
-        self.assertEquals(action1.attack_bonus, 9)
-        self.assertEquals(action2.name, "Tail")
-        self.assertEquals(action1.damage_dice, "3d6")
-        self.assertEquals(action1.attack_bonus, 9)
+        self.assertEquals(action1.description, "The aboleth makes three tentacle attacks.")
+        self.assertEquals(action1.damage_dice, "")
+        self.assertEquals(action1.attack_bonus, 0)
+        self.assertEquals(action1.damage_bonus, 0)
+        self.assertEquals(action4.name, "Enslave (3/day)")
+        self.assertEquals(action4.description, "The aboleth targets one creature it can see within 30 ft. of it. "
+                                               "The target must succeed on a DC 14 Wisdom saving throw or be magically "
+                                               "charmed by the aboleth until the aboleth dies or until it is on a "
+                                               "different plane of existence from the target. The charmed target is "
+                                               "under the aboleth's control and can't take reactions, and the aboleth "
+                                               "and the target can communicate telepathically with each other over any "
+                                               "distance. \nWhenever the charmed target takes damage, the target can "
+                                               "repeat the saving throw. On a success, the effect ends. No more than "
+                                               "once every 24 hours, the target can also repeat the saving throw when "
+                                               "it is at least 1 mile away from the aboleth.")
+        self.assertEquals(action2.name, "Tentacle")
+        self.assertEquals(action2.attack_bonus, 9)
+        self.assertEquals(action2.damage_bonus, 5)
+        self.assertEquals(action2.damage_dice, "2d6")
+
