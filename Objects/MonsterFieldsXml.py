17335b48b556b783812ea2badecd554d4b81aac7
Chris Mattera <chris.mattera@comcast.com>
2017-05-10 17:18:09 -0700

    Added tests in XmlMonsterFileParserTest. Started implimenting XmlMonsterFileParser. Refactored MonsterFieldsXml. Started converting usage of MonsterFieldsXml.

--- Objects/MonsterFieldsXml.py
+++ Objects/MonsterFieldsXml.py
@@ -1,30 +1,22 @@
-
-class MonsterFieldsXml(object):
-    def __init__(self):
-        """Types"""
-        self.XML = "xml"
-        self.JSON = "json"
-
-        """Basic Fields"""
-        self.NAME = 'name'
-        self.TYPE = 'type'
-        self.SIZE = 'size'
-
-        """Complex Fields"""
-        self.SPECIAL_ABILITIES = MonsterFieldsSpecialAbilities()
-        self.ACTIONS = MonsterFieldsActions()
-        self.LEGENDARY_ACTIONS = MonsterLegendaryActions()
-
-
 class MonsterFieldsSpecialAbilities(object):
+    ROOT = 'trait'
+    NAME = 'name'
+    DESCRIPTION = 'text'
+
     def __init__(self):
-        """Special Abilities"""
         self.ROOT = 'trait'
         self.NAME = 'name'
         self.DESCRIPTION = 'text'
 
 
 class MonsterFieldsActions(object):
+    ROOT = 'action'
+    NAME = 'name'
+    DESCRIPTION = 'text'  # could be multiple nodes that need to be stitched together
+    DAMAGE_DICE = 'attack'  # need to parse out this field
+    DAMAGE_BONUS = 'attack'  # need to parse out this field
+    ATTACK_BONUS = 'attack'  # need to parse out this field
+
     def __init__(self):
         self.ROOT = 'action'
         self.NAME = 'name'
@@ -35,6 +27,13 @@ class MonsterFieldsActions(object):
 
 
 class MonsterLegendaryActions(object):
+    ROOT = 'legendary'
+    NAME = 'name'
+    DESCRIPTION = 'text'  # could be multiple nodes that need to be stitched together
+    DAMAGE_DICE = 'attack'  # need to parse out this field
+    DAMAGE_BONUS = 'attack'  # need to parse out this field
+    ATTACK_BONUS = 'attack'  # need to parse out this field
+
     def __init__(self):
         self.ROOT = 'legendary'
         self.NAME = 'name'
@@ -43,3 +42,33 @@ class MonsterLegendaryActions(object):
         self.DAMAGE_BONUS = 'attack'  # need to parse out this field
         self.ATTACK_BONUS = 'attack'  # need to parse out this field
 
+
+class MonsterFieldsXml(object):
+    MONSTER_TAG = "monster"
+    ROOT_TAG = "compendium"
+
+    """Basic Fields"""
+    NAME = 'name'
+    TYPE = 'type'
+    SIZE = 'size'
+
+    """Complex Fields"""
+    SPECIAL_ABILITIES = MonsterFieldsSpecialAbilities()
+    ACTIONS = MonsterFieldsActions()
+    LEGENDARY_ACTIONS = MonsterLegendaryActions()
+
+    def __init__(self):
+        self.XML = "xml"
+        self.JSON = "json"
+        self.MONSTER_TAG = "monster"
+        self.ROOT_TAG = "compendium"
+
+        """Basic Fields"""
+        self.NAME = 'name'
+        self.TYPE = 'type'
+        self.SIZE = 'size'
+
+        """Complex Fields"""
+        self.SPECIAL_ABILITIES = MonsterFieldsSpecialAbilities()
+        self.ACTIONS = MonsterFieldsActions()
+        self.LEGENDARY_ACTIONS = MonsterLegendaryActions()
--- Test/Parser/XmlMonsterFileParserTest.py
+++ Test/Parser/XmlMonsterFileParserTest.py
@@ -1,6 +1,7 @@
 import unittest
 import os
 import definitions
+import xml.etree.ElementTree as etree
 from Parser.XmlMonsterFileParser import XmlMonsterFileParser
 
 
@@ -8,10 +9,22 @@ class XmlMonsterFileParserTest(unittest.TestCase):
 
     def setUp(self):
         self.monster_xml = os.path.join(definitions.XML_RESOURCES_PATH, '5e-SRD-Monsters[FIGHTCLUB].xml')
-        print(self.monster_xml)
+        self.invalid_xml = os.path.join(definitions.XML_RESOURCES_PATH, 'InvalidXml.xml')
         self.file_parser = XmlMonsterFileParser(self.monster_xml)
 
     def test_get_monster_list_from_file(self):
-        result = self.file_parser.get_monster_list_from_file()
-        self.assertEquals(len(result), 325)
+        empty_parser = XmlMonsterFileParser()
+        monster_list = empty_parser.get_monster_list_from_file()
+        self.assertEquals(len(monster_list), 0)
 
+        monster_list = self.file_parser.get_monster_list_from_file()
+        self.assertEquals(len(monster_list), 6)
+
+    def test_get_tree_from_file(self):
+        self.assertNotEquals(self.file_parser.get_tree_from_file(self.monster_xml), None)
+
+        with self.assertRaises(FileNotFoundError):
+            self.file_parser.get_tree_from_file("/I/Dont/Exist.xml")
+
+        with self.assertRaises(etree.ParseError):
+            self.file_parser.get_tree_from_file(self.invalid_xml)
